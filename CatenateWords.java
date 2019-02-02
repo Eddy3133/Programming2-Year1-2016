83abff54198e4990d88a7859d10d61edf361d3bb
Edirin Okpikpi <pleasureokpikpi@gmail.com>
2019-02-01 18:12:53 -0600

    Add files via upload

---
+++ CatenateWords.java
@@ -0,0 +1,24 @@
+/**
+ * Name: Edirin Okpikpi
+ * Student Number:3078829
+ * Lab: 1
+ * 
+ */
+public class CatenateWords
+{
+    public static void main ( String [] args ){
+        String course = "retail sales tax";
+        String result = formNewWord(course);
+        System.out.println (result);
+    }
+
+    public static String formNewWord ( String line ){
+        String newWord = "  ";
+        // for each word
+        for ( String s: line.split (" "))
+        // catenate to previous result
+            newWord +=
+            Character.toUpperCase(s.charAt (0)) + s.substring(1).toLowerCase();
+        return newWord ;
+    }
+}
